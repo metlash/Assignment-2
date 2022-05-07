578ee34cb2f96bf1fe712d0455d578b59fe90aa0
metlash <51310264+metlash@users.noreply.github.com>
2022-05-07 12:28:35 +0530

    Add files via upload

---
+++ Bus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BusTravels.Models
+{
+    public class Bus
+    {
+        [Key]
+        [Required]
+        public int BusId { get; set; }
+
+
+        [Required]
+
+        public string BoardingPoint { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+
+        public DateTime TravelDate { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,45 @@
+﻿using BusTravels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusTravels.Controllers
+{
+    public class HomeController : Controller
+    {
+        BusDbContext context = new BusDbContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(context.Buses);
+        }
+
+
+        [HttpPost]
+        public ViewResult Index(Bus model)
+        {
+            //Code to save the customer data here
+            ViewData["error"] = "Customer Data Update successfully";
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        public ActionResult CreateAccount(Bus model)
+        {
+            context.Buses.Add(model);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
