22692188164c22cd38158cd6113e27cba14b40d2
metlash <51310264+metlash@users.noreply.github.com>
2022-05-09 19:30:12 +0530

    Add files via upload

---
+++ Migrations/202205070638484_Create.cs
@@ -0,0 +1,29 @@
+﻿namespace BusTravels.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Create : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Buses",
+                c => new
+                    {
+                        BusId = c.Int(nullable: false, identity: true),
+                        BoardingPoint = c.String(nullable: false),
+                        TravelDate = c.DateTime(nullable: false),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Rating = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BusId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Buses");
+        }
+    }
+}
---
+++ Models/BusDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BusTravels.Models
+{
+    public class BusDbContext : DbContext
+    {
+        public BusDbContext()
+            : base("name=conn")
+        {
+
+        }
+
+        public DbSet<Bus> Buses { get; set; }
+    }
+}
